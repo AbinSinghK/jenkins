93a822e39cc5ceadadf9bf514268da15a3870783
AbinSinghK <141057162+AbinSinghK@users.noreply.github.com>
2023-09-26 18:41:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,12 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "us-south-1"
 }
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "mum"
-}
 
 variable "instance_type" {
   description = "instance type for ec2"
   default     =  "t2.micro"
-}
-
-variable "security_group" {
-  description = "Name of security group"
-  default     = "All Traffic"
 }
 
 variable "tag_name" {
@@ -24,6 +15,6 @@ variable "tag_name" {
 }
 
 variable "ami_id" {
-  description = "AMI for Amazon Ec2 instance"
-  default     = "ami-0ff30663ed13c2290"
+  description = "AMI for linux Ec2 instance"
+  default     = "ami-0ff30663ed13c2290 "
 }
