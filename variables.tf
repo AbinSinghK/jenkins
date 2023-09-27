174bdf349b7bd04f5a3aa06e72c5d062e05c10ba
AbinSinghK <141057162+AbinSinghK@users.noreply.github.com>
2023-09-27 11:04:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,27 @@
-variable "aws_region" {
-       description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+variable "location" {
+    default = "ap-south-1"
 }
 
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "mum" 
+variable "os_name" {
+    default = "ami-09ba48996007c8b50"
 }
 
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
+variable "key" {
+    default = "mum"
 }
 
-variable "security_group" { 
-    description = "Name of security group" 
-    default     = "All Traffic" 
+variable "instance-type" {
+    default = "t2.small"
 }
 
-variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
-} 
-variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0f5ee92e2d63afc18" 
+variable "vpc-cidr" {
+    default = "10.10.0.0/16"  
+}
+
+variable "subnet1-cidr" {
+    default = "10.10.1.0/24"
+  
+}
+variable "subent_az" {
+    default =  "ap-south-1a"  
 }
